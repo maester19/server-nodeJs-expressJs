2302e71cb174d88fd7fee5f794c1bcd65f9eb9ba
maesterlyb <yves.ekanga27@gmail.com>
2022-08-27 18:11:20 +0100

    [update]: ajust login mongoDB path

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const stuffRoutes = require("./routes/stuff")
 
-mongoose.connect('mongo',
+mongoose.connect('mongodb://localhost:27017/db_test',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
