ff65375a12bb275683f7e3fd7981bfe9bdd6e40f
maesterlyb <yves.ekanga27@gmail.com>
2022-10-31 18:31:44 +0100

    [feat]: add user road, controller and model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const stuffRoutes = require("./routes/stuff")
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb://localhost:27017/db_test',
   { useNewUrlParser: true,
@@ -19,5 +20,6 @@ app.use((req, res, next) => {
   });
 
 app.use("/api/stuff", stuffRoutes)
+app.use("/api/auth", userRoutes)
 
 module.exports = app;
